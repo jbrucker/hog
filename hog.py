b02b8f9a92c90b8ec92dad94019b4cbaf84deb0c
jbrucker <jebrucker@gmail.com>
2021-01-07 14:37:22 +0700

    checkin solution for phase 1

---
+++ hog.py
@@ -0,0 +1,376 @@
+"""CS 61A Presents The Game of Hog."""
+
+from dice import six_sided, four_sided, make_test_dice
+from ucb import main, trace, interact
+from math import gcd
+
+# The rules of 'hog' on ComposingPrograms.com (cs61a/fa13) are
+# different from the runs in https://cs61a.org. 
+# Set NEWHOG to True to use rules from https://cs61a.org
+NEWHOG = False
+
+GOAL_SCORE = 100  # The goal of Hog is to score 100 points.
+
+######################
+# Phase 1: Simulator #
+######################
+
+
+def roll_dice(num_rolls, dice=six_sided):
+    """Simulate rolling the DICE exactly NUM_ROLLS > 0 times. Return the sum of
+    the outcomes unless any of the outcomes is 1. In that case, return 1.
+    Your implementation should always roll the dice *exactly* num_rolls times.
+
+    num_rolls:  The number of dice rolls that will be made.
+    dice:       A function that simulates a single dice roll outcome.
+    """
+    # These assert statements ensure that num_rolls is a positive integer.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls > 0, 'Must roll at least once.'
+    # BEGIN PROBLEM 1
+    sum_dice = 0
+    # Can't early return from the loop when a 1 is encountered,
+    # so we need to keep track if any 1s.
+    got_one = False
+    for roll in range(0, num_rolls):
+        value = dice()
+        got_one = got_one or value == 1
+        sum_dice += value
+    # stupid rule
+    return 1 if got_one else sum_dice 
+    # END PROBLEM 1
+
+
+def free_bacon(score):
+    """Return the points scored from rolling 0 dice (Free Bacon).
+
+    score:  The opponent's current score.
+    """
+    assert score < GOAL_SCORE, 'The game should be over.'
+    assert score >= 0, 'Negative score is not allowed.'
+    if NEWHOG: return new_free_bacon(score)
+
+    max_digit = 0
+    while score > 0:
+        max_digit = max(max_digit, score%10)
+        score = score//10
+    return max_digit + 1
+
+
+def take_turn(num_rolls, opponent_score, dice=six_sided):
+    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
+    Return the points scored for the turn by the current player.
+
+    num_rolls:       The number of dice rolls that will be made.
+    opponent_score:  The total score of the opponent.
+    dice:            A function that simulates a single dice roll outcome.
+    """
+    # Leave these assert statements here; they help check for errors.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
+    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
+    assert opponent_score < GOAL_SCORE, 'The game should be over.'
+    # BEGIN PROBLEM 3
+    if num_rolls == 0:
+        return free_bacon(opponent_score)
+    return roll_dice(num_rolls, dice=dice)
+    # END PROBLEM 3
+
+# Playing a game
+
+def select_dice(player_score, opponent_score):
+    """Select six-sided dice unless the sum of player_score and opponent_score 
+    is a multiple of 7, in which case select four-sided dice (Hog wild).
+
+    >>> select_dice(4, 24) == four_sided
+    True
+    >>> select_dice(16, 64) == six_sided
+    True
+    >>> select_dice(0, 0) == four_sided
+    True
+    """
+    if (player_score + opponent_score)%7 == 0:
+        return four_sided
+    return six_sided
+
+def swine_swap(score0, score1):
+    """Return True if player's should exchange scores."""
+    return score0 == 2*score1 or score1 == 2*score0
+
+
+def other(player):
+    """Return the other player, for a player numbered 0 or 1.
+
+    >>> other(0)
+    1
+    >>> other(1)
+    0
+    """
+    return 1 - player
+
+
+def next_player(player: int, player_score: int, opponent_score: int):
+    """Return the id of the next player who should take a turn."""
+    return other(player)
+
+
+def play(strategy0, strategy1, goal=GOAL_SCORE, score0=0, score1=0):
+    """Simulate a game and return the final scores of both players, with Player
+    0's score first, and Player 1's score second.
+
+    A strategy is a function that takes two total scores as arguments (the
+    current player's score, and the opponent's score), and returns a number of
+    dice that the current player will roll this turn.
+
+    strategy0:  The strategy function for Player 0, who plays first.
+    strategy1:  The strategy function for Player 1, who plays second.
+    score0:     Starting score for Player 0
+    score1:     Starting score for Player 1
+    goal:       The game ends and someone wins when this score is reached.
+    """
+    # Use a list so we can use same code for both players' turns.
+    # This violates a design principle to avoid parallel arrays.
+    score = [score0, score1]
+    strategy = [strategy0, strategy1]
+    # the current player
+    player = 0
+    while score[0] < goal and score[1] < goal:
+        opponent = other(player)
+        num_dice = strategy[player](score[player], score[opponent])
+        dice = select_dice(score[player], score[opponent])
+        score_this_turn = take_turn(num_dice, score[opponent], dice)
+        score[player] += score_this_turn
+        if swine_swap(score[player], score[opponent]):
+            score = [score[1], score[0]]
+        say_scores(score[0], score[1])
+        player = next_player(player, score[player], score[opponent])
+    # game over.
+
+    return score[0], score[1]
+
+
+#######################
+# Phase 2: Commentary #
+#######################
+
+def say_scores(score0, score1):
+    """A commentary function that announces the score for each player."""
+    print("Player 0 now has", score0, "and Player 1 now has", score1)
+    return say_scores
+
+
+def announce_lead_changes(last_leader=None):
+    """Return a commentary function that announces lead changes.
+
+    >>> f0 = announce_lead_changes()
+    >>> f1 = f0(5, 0)
+    Player 0 takes the lead by 5
+    >>> f2 = f1(5, 12)
+    Player 1 takes the lead by 7
+    >>> f3 = f2(8, 12)
+    >>> f4 = f3(8, 13)
+    >>> f5 = f4(15, 13)
+    Player 0 takes the lead by 2
+    """
+    def say(score0, score1):
+        if score0 > score1:
+            leader = 0
+        elif score1 > score0:
+            leader = 1
+        else:
+            leader = None
+        if leader != None and leader != last_leader:
+            print('Player', leader, 'takes the lead by', abs(score0 - score1))
+        return announce_lead_changes(leader)
+    return say
+
+
+def both(f, g):
+    """Return a commentary function that says what f says, then what g says.
+
+    NOTE: the following game is not possible under the rules, it's just
+    an example for the sake of the doctest
+
+    >>> h0 = both(say_scores, announce_lead_changes())
+    >>> h1 = h0(10, 0)
+    Player 0 now has 10 and Player 1 now has 0
+    Player 0 takes the lead by 10
+    >>> h2 = h1(10, 8)
+    Player 0 now has 10 and Player 1 now has 8
+    >>> h3 = h2(10, 17)
+    Player 0 now has 10 and Player 1 now has 17
+    Player 1 takes the lead by 7
+    """
+    def say(score0, score1):
+        return both(f(score0, score1), g(score0, score1))
+    return say
+
+
+def announce_highest(who, last_score=0, running_high=0):
+    """Return a commentary function that announces when WHO's score
+    increases by more than ever before in the game.
+
+    NOTE: the following game is not possible under the rules, it's just
+    an example for the sake of the doctest
+
+    >>> f0 = announce_highest(1) # Only announce Player 1 score gains
+    >>> f1 = f0(12, 0)
+    >>> f2 = f1(12, 9)
+    9 point(s)! The most yet for Player 1
+    >>> f3 = f2(20, 9)
+    >>> f4 = f3(20, 30)
+    21 point(s)! The most yet for Player 1
+    >>> f5 = f4(20, 47) # Player 1 gets 17 points; not enough for a new high
+    >>> f6 = f5(21, 47)
+    >>> f7 = f6(21, 77)
+    30 point(s)! The most yet for Player 1
+    """
+    assert who == 0 or who == 1, 'The who argument should indicate a player.'
+    # BEGIN PROBLEM 7
+    "*** YOUR CODE HERE ***"
+    # END PROBLEM 7
+
+
+#######################
+# Phase 3: Strategies #
+#######################
+
+
+def always_roll(n):
+    """Return a strategy that always rolls N dice.
+
+    A strategy is a function that takes two total scores as arguments (the
+    current player's score, and the opponent's score), and returns a number of
+    dice that the current player will roll this turn.
+
+    >>> strategy = always_roll(5)
+    >>> strategy(0, 0)
+    5
+    >>> strategy(99, 99)
+    5
+    """
+    def strategy(score, opponent_score):
+        return n
+    return strategy
+
+
+def make_averaged(original_function, trials_count=1000):
+    """Return a function that returns the average value of ORIGINAL_FUNCTION
+    when called.
+
+    To implement this function, you will have to use *args syntax, a new Python
+    feature introduced in this project.  See the project description.
+
+    >>> dice = make_test_dice(4, 2, 5, 1)
+    >>> averaged_dice = make_averaged(dice, 1000)
+    >>> averaged_dice()
+    3.0
+    """
+    # BEGIN PROBLEM 8
+    "*** YOUR CODE HERE ***"
+    # END PROBLEM 8
+
+
+def max_scoring_num_rolls(dice=six_sided, trials_count=1000):
+    """Return the number of dice (1 to 10) that gives the highest average turn
+    score by calling roll_dice with the provided DICE over TRIALS_COUNT times.
+    Assume that the dice always return positive outcomes.
+
+    >>> dice = make_test_dice(1, 6)
+    >>> max_scoring_num_rolls(dice)
+    1
+    """
+    # BEGIN PROBLEM 9
+    "*** YOUR CODE HERE ***"
+    # END PROBLEM 9
+
+
+def winner(strategy0, strategy1):
+    """Return 0 if strategy0 wins against strategy1, and 1 otherwise."""
+    score0, score1 = play(strategy0, strategy1)
+    if score0 > score1:
+        return 0
+    else:
+        return 1
+
+
+def average_win_rate(strategy, baseline=always_roll(6)):
+    """Return the average win rate of STRATEGY against BASELINE. Averages the
+    winrate when starting the game as player 0 and as player 1.
+    """
+    win_rate_as_player_0 = 1 - make_averaged(winner)(strategy, baseline)
+    win_rate_as_player_1 = make_averaged(winner)(baseline, strategy)
+
+    return (win_rate_as_player_0 + win_rate_as_player_1) / 2
+
+
+def run_experiments():
+    """Run a series of strategy experiments and report results."""
+    if True:  # Change to False when done finding max_scoring_num_rolls
+        six_sided_max = max_scoring_num_rolls(six_sided)
+        print('Max scoring num rolls for six-sided dice:', six_sided_max)
+
+    if False:  # Change to True to test always_roll(8)
+        print('always_roll(8) win rate:', average_win_rate(always_roll(8)))
+
+    if False:  # Change to True to test bacon_strategy
+        print('bacon_strategy win rate:', average_win_rate(bacon_strategy))
+
+    if False:  # Change to True to test extra_turn_strategy
+        print('extra_turn_strategy win rate:', average_win_rate(extra_turn_strategy))
+
+    if False:  # Change to True to test final_strategy
+        print('final_strategy win rate:', average_win_rate(final_strategy))
+
+    "*** You may add additional experiments as you wish ***"
+
+
+
+def bacon_strategy(score, opponent_score, cutoff=8, num_rolls=6):
+    """This strategy rolls 0 dice if that gives at least CUTOFF points, and
+    rolls NUM_ROLLS otherwise.
+    """
+    # BEGIN PROBLEM 10
+    return 6  # Replace this statement
+    # END PROBLEM 10
+
+
+def extra_turn_strategy(score, opponent_score, cutoff=8, num_rolls=6):
+    """This strategy rolls 0 dice when it triggers an extra turn. It also
+    rolls 0 dice if it gives at least CUTOFF points and does not give an extra turn.
+    Otherwise, it rolls NUM_ROLLS.
+    """
+    # BEGIN PROBLEM 11
+    return 6  # Replace this statement
+    # END PROBLEM 11
+
+
+def final_strategy(score, opponent_score):
+    """Write a brief description of your final strategy.
+
+    *** YOUR DESCRIPTION HERE ***
+    """
+    # BEGIN PROBLEM 12
+    return 6  # Replace this statement
+    # END PROBLEM 12
+
+##########################
+# Command Line Interface #
+##########################
+
+# NOTE: Functions in this section do not need to be changed. They use features
+# of Python not yet covered in the course.
+
+
+@main
+def run(*args):
+    """Read in the command-line argument and calls corresponding functions."""
+    import argparse
+    parser = argparse.ArgumentParser(description="Play Hog")
+    parser.add_argument('--run_experiments', '-r', action='store_true',
+                        help='Runs strategy experiments')
+
+    args = parser.parse_args()
+
+    if args.run_experiments:
+        run_experiments()
---
+++ test/test_phase1.py
@@ -0,0 +1,92 @@
+"""Unit tests for Phase 1 of the Hog project in "Composing Programs"
+   as described on
+   https://inst.eecs.berkeley.edu//~cs61a/fa13/proj/hog/hog.html
+   Note that the requirements and starter code for this project
+   are _not_ the same as in the version on cs61a.org.
+
+Requires:
+  The Python 'parameterized' package.  
+  Install it using 'pip install parameterized'
+
+How to run tests:
+  In your hog project directory (parent of this directory), type:
+
+  python3 -m unittest test/test_phase1.py
+
+How to run the 'doctest' tests contained in hog.py:
+  python3 -m doctest hog.py
+
+Or, for more verbose output that shows all doctests being run:
+  python3 -m doctest -v hog.py
+"""
+
+import unittest
+from parameterized import parameterized, parameterized_class
+from hog import *
+from dice import make_fair_dice, make_test_dice
+
+class TestPhase1(unittest.TestCase):
+
+    def test_roll_dice(self):
+        """roll_dice() should return sum of several 'rolls' of dice"""
+        test_dice = make_test_dice(6, 5, 2, 3, 4, 5, 6, 6, 6, 6)
+        self.assertEqual(6, roll_dice(1, dice=test_dice))
+        self.assertEqual(7, roll_dice(2, dice=test_dice))
+        self.assertEqual(12, roll_dice(3, dice=test_dice))
+        self.assertEqual(24, roll_dice(4, dice=test_dice))
+        self.assertEqual(49, roll_dice(10, dice=test_dice))
+
+    def test_roll_dice_with_one(self):
+        """Test roll_dice() when a 1 appears on at least one die"""
+        test_dice = make_test_dice(1, 5, 1, 3, 1, 2, 3, 4, 5, 6)
+        self.assertEqual(1, roll_dice(1, dice=test_dice))
+        self.assertEqual(1, roll_dice(3, dice=test_dice))
+        self.assertEqual(1, roll_dice(5, dice=test_dice))
+        self.assertEqual(1, roll_dice(10, dice=test_dice))
+
+    def test_free_bacon(self):
+        """free bacon should give a score 1 + max digit in opponent score"""
+        for score in range(0,10):
+            expect = score+1
+            self.assertEqual(expect, free_bacon(score),
+                             f"free_bacon({score}) should be {expect}")
+        self.assertEqual(2, free_bacon(10))
+        self.assertEqual(2, free_bacon(11))
+        self.assertEqual(3, free_bacon(12))
+        self.assertEqual(3, free_bacon(21))
+        self.assertEqual(3, free_bacon(22))
+        self.assertEqual(6, free_bacon(54))
+        self.assertEqual(6, free_bacon(25))
+        self.assertEqual(6, free_bacon(50))
+        self.assertEqual(10, free_bacon(39))
+        self.assertEqual(10, free_bacon(98))
+        self.assertEqual(10, free_bacon(90))
+
+    def test_take_turn(self):
+        """take a turn using different opponent scores and dice"""
+        FACE_VALUE = 4
+        test_dice = make_test_dice(FACE_VALUE) # always rolls same value
+        # test the free bacon case
+        self.assertEqual(10, take_turn(0, 29, dice=test_dice))
+        self.assertEqual(8, take_turn(0, 75, dice=test_dice))
+        self.assertEqual(1, take_turn(0, 0, dice=test_dice))
+        # for rolls > 0 opponent score should be irrelevant
+        self.assertEqual(FACE_VALUE, take_turn(1, 0, dice=test_dice))
+        self.assertEqual(3*FACE_VALUE, take_turn(3, 0, dice=test_dice))
+        self.assertEqual(3*FACE_VALUE, take_turn(3, 23, dice=test_dice))
+        self.assertEqual(10*FACE_VALUE, take_turn(10, 23, dice=test_dice))
+        # this case always gets a score of 1
+        test_dice = make_test_dice(1, 1, 1, 1, 1)
+        self.assertEqual(1, take_turn(1, 10, dice=test_dice))
+        self.assertEqual(1, take_turn(2, 10, dice=test_dice))
+        self.assertEqual(1, take_turn(3, 10, dice=test_dice))
+        self.assertEqual(1, take_turn(9, 10, dice=test_dice))
+        self.assertEqual(1, take_turn(10, 10, dice=test_dice))
+        # a test die with 2 values to verify student code uses it
+        FACE1 = 2
+        FACE2 = 5
+        test_dice = make_test_dice(FACE1, FACE2)
+        self.assertEqual(FACE1, take_turn(1, 10, dice=test_dice))
+        self.assertEqual(FACE2, take_turn(1, 10, dice=test_dice))
+        self.assertEqual(FACE1+FACE2, take_turn(2, 10, dice=test_dice))
+        self.assertEqual(2*FACE1+FACE2, take_turn(3, 10, dice=test_dice))
